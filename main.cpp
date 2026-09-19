2e595d0d0f1ce513502b50f9e48fdcc1f7991f94
kstudilova <kstudilova@gmail.com>
2026-09-20 01:53:03 +0300

    calc monte carlo

--- main.cpp
+++ main.cpp
@@ -1,4 +1,33 @@
 #include <iostream>
+#include <cstddef>
+#include <random>
+
+bool isInside(double x, double y, double r)
+{
+  double a = x - r;
+  double b = y - r;
+  return a * a + b * b <= r * r;
+}
+
+size_t calc(double r, size_t tests, size_t seed)
+{
+  size_t count = 0;
+
+  std::default_random_engine engine(seed);
+  std::uniform_real_distribution< double > dist(0.0, 2.0 * r);
+
+  for (size_t i = 0; i < tests; ++i)
+  {
+    double x = dist(engine);
+    double y = dist(engine);
+
+    if (isInside(x, y, r))
+    {
+      ++count;
+    }
+  }
+  return count;
+}
 
 int main()
 {
